79ece125ac563054a4e4284a9ffc40e3dd400d58
Aldyoga <sandiali703@gmail.com>
2022-11-03 15:39:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+    <div className="Parentbox">
+      <div className="Foto">
+        <img src="bmw.jpg" />
+      </div>
+      <div className="Deskripsi">
+        <p className="Cate">Cars</p>
+        <h1 className="Title">Cars Modified</h1>
+        <p className="Price">IDR 1.3xx.xxx.xxx</p>
+        <p className="Info">
+          BMW (singkatan untuk Bayerische Motoren Werke, atau dalam Bahasa Inggris, Bavarian Motor Works), adalah sebuah perusahaan otomotif Jerman yang memproduksi mobil dan sepeda motor. BMW didirikan pada tahun 1916 oleh Franz Josef Popp. BMW AG adalah perusahaan induk dari merk mobil MINI dan Rolls-Royce, dan, dulunya Rover.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="Size">TYPE</h1>
+        <div className="Ukuran">
+          <button className="S">E350</button>
+        </div>
+        <h1 className="Tombol">Beli Sekarang</h1>
+        <div className="Beli">
+          <button className="Buy Now">Buy Now</button>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
